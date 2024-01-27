2c508fd2a7382828eca4cb79d0c380c99d69616d
DRAKUTONT <grudnezlov@gmail.com>
2024-01-27 16:35:03 +0300

    add prize image

--- backend/sea_battle/game/models.py
+++ backend/sea_battle/game/models.py
@@ -79,7 +79,7 @@ class Cell(models.Model):
             (3, "Стоит уничтоженный корабль"),
         ),
         verbose_name="статус",
-        default=1,
+        default=0,
     )
 
     shot_by = models.ForeignKey(
@@ -107,6 +107,9 @@ class Cell(models.Model):
 
 
 class Prize(models.Model):
+    def prize_imgae_upload_to(self, filename):
+        return f"prize/{self.id}/{filename}"
+
     title = models.CharField(
         verbose_name="название",
         max_length=150,
@@ -114,6 +117,13 @@ class Prize(models.Model):
 
     text = models.TextField(
         verbose_name="описание",
+    )
+
+    image = models.ImageField(
+        upload_to=prize_imgae_upload_to,
+        verbose_name="изображение приза",
+        null=True,
+        blank=True,
     )
 
     winner = models.ForeignKey(
--- backend/sea_battle/sea_battle/urls.py
+++ backend/sea_battle/sea_battle/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -18,4 +19,9 @@ if settings.DEBUG:
             "__debug__/",
             include(debug_toolbar.urls),
         ),
+    )
+
+    urlpatterns += static.static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
     )
