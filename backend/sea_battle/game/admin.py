e190b4c4defe70ba9c2838f3e7b44bec6cf16d50
DRAKUTONT <grudnezlov@gmail.com>
2024-01-31 22:52:28 +0300

    add filters in django admin panel

--- backend/sea_battle/game/admin.py
+++ backend/sea_battle/game/admin.py
@@ -3,8 +3,22 @@ from django.contrib import admin
 from game import models
 
 
-admin.site.register(models.Game, readonly_fields=["link"])
-admin.site.register(models.Prize)
 admin.site.register(models.UserShots)
 admin.site.register(models.Ship)
 admin.site.register(models.Cell)
+
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ["title", "link", "status"]
+    readonly_fields = ["link", "finish_at"]
+
+    filter_horizontal = ["users"]
+
+    list_filter = ["status"]
+
+
+@admin.register(models.Prize)
+class PrizeAdmin(admin.ModelAdmin):
+    list_filter = ("winner", "game")
+
