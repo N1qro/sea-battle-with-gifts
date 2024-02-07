de0af4be47acd5d5bf2f0a5efc3501a64d158a86
Andrew <andrey777-novikov777@yandex.ru>
2024-02-07 19:33:07 +0300

    Added Homepage v1

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,44 @@
+import { FlexRow } from "../styles/GlobalStyles"
+import { MarginSection, Card, AttentionTitle, Grid2Row } from "../styles/HomePage"
+import { RegularText } from "../styles/TextStyles"
+
 function Home() {
 	return (
 		<div>
-			das
+			<MarginSection>
+				<Card>
+					<AttentionTitle>Тот самый морской бой!</AttentionTitle>
+					<p>Только с призами 🎁</p>
+				</Card>
+			</MarginSection>
+			<MarginSection>
+				<Grid2Row>
+				<Card>
+					<RegularText>
+						Получи возможность выиграть игровые
+						подарочные карты или цифровые призы играя
+						в морской бой
+					</RegularText>
+				</Card>
+				<Card>
+					<RegularText>
+						Зарегистрируй аккаунт с той же почтой, как на ... ,
+						покупай продукцию ... и получи возможность попасть на игру.
+						В каждой игре у абсолютно каждого есть возможность выиграть
+					</RegularText>
+				</Card>
+				</Grid2Row>
+			</MarginSection>
+			<MarginSection>
+				<p>Наши спонсоры</p>
+				<Card>
+					<FlexRow>
+						<p>Gamerzz</p>
+						<p>Battleship</p>
+						<p>JustGive</p>
+					</FlexRow>
+				</Card>
+			</MarginSection>
 		</div>
 	)
 }
--- frontend/src/styles/HomePage.tsx
+++ frontend/src/styles/HomePage.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { RegularText } from "./TextStyles";
 
 export const Container = styled.div`
     
@@ -14,4 +15,36 @@ export const SectionTransparentBackground = styled.div`
     border-radius: 19px;
     background: rgba(217, 217, 217, 0.50);
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
+
+export const MarginSection = styled.section`
+    margin-block: 2rem;
+`
+
+export const Card = styled.div`
+    color: black;
+    padding: 20px 30px;
+    text-align: center;
+    background-color: #D9D9D9;
+    border-radius: 19px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+`
+
+export const AttentionTitle = styled.h1`
+    font-size: 2.5rem;
+    background: linear-gradient(to right, #121FCF 0%, #CF1512 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+`
+
+export const Grid2Row = styled.div`
+    & > ${Card} {
+        display: flex;
+        align-items: center;
+    };
+
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: repeat(2, minmax(300px, 1fr));
+    grid-template-rows: minmax(300px, 1fr);
 `
